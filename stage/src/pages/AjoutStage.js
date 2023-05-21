6a5237ec8480738302b2e6b6389ef2a2e6697aaa
jay23 <jay23@DESKTOP-KQDTLB9>
2023-05-21 00:44:37 -0400

    Ajout stage et etudiant fonction + connection base
    Co-authored-by: DavidBucin <DavidBucin@users.noreply.github.com>
    Co-authored-by: Theyoungsleek <Theyoungsleek@users.noreply.github.com>

--- stage/src/pages/AjoutStage.js
+++ stage/src/pages/AjoutStage.js
@@ -1,145 +1,172 @@
 import React, { useState } from 'react';
 
-const FormulaireAjoutStage = () => {
-  const [nomContact, setNomContact] = useState('');
-  const [courrielContact, setCourrielContact] = useState('');
-  const [telephoneContact, setTelephoneContact] = useState('');
-  const [nomEntreprise, setNomEntreprise] = useState('');
-  const [adresseEntreprise, setAdresseEntreprise] = useState('');
-  const [typeStage, setTypeStage] = useState('');
-  const [nombrePostes, setNombrePostes] = useState('');
-  const [descriptionStage, setDescriptionStage] = useState('');
-  const [remuneration, setRemuneration] = useState('');
+const FormulaireAjoutEmployeur = () => {
+  const [donneesEmployeur, setDonneesEmployeur] = useState({
+    nomContact: '',
+    courrielContact: '',
+    telephoneContact: '',
+    nomEntreprise: '',
+    adresseEntreprise: '',
+    typeStage: '',
+    nombrePostes: 1,
+    descriptionStage: '',
+    remuneration: 0,
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    // Perform validation
-    if (!/^[a-z\s]+$/i.test(nomContact)) {
-      alert('Le nom du contact doit comporter uniquement des lettres');
-      return;
-    }
-    
-    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(courrielContact)) {
-      alert('Le courriel est dans un format invalide');
-      return;
-    }
-
-    if (!/^\+?\d{10,15}$/.test(telephoneContact)) {
-      alert('Le numéro de téléphone est dans un format invalide');
-      return;
-    }
-
-    if (nombrePostes <= 0) {
-      alert('Le nombre de postes doit être supérieur à zéro');
-      return;
-    }
-
-    if (!isNaN(parseFloat(remuneration)) && parseFloat(remuneration) <= 0) {
-      alert('La rémunération doit être un nombre supérieur à zéro');
-      return;
-    }
-    
-    
-    alert('Stage added successfully!');
-   
-    setNomContact('');
-    setCourrielContact('');
-    setTelephoneContact('');
-    setNomEntreprise('');
-    setAdresseEntreprise('');
-    setTypeStage('');
-    setNombrePostes('');
-    setDescriptionStage('');
-    setRemuneration('');
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDonneesEmployeur((donneesPrecedentes) => ({
+      ...donneesPrecedentes,
+      [name]: value,
+    }));
   };
 
+  const buttonAjouterClick = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3000/employeurs', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(donneesEmployeur)
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || "Couldn't add employer.");
+      }
+
+      alert("Employeur ajouté avec succès!");
+
+      setDonneesEmployeur({
+        nomContact: '',
+        courrielContact: '',
+        telephoneContact: '',
+        nomEntreprise: '',
+        adresseEntreprise: '',
+        typeStage: '',
+        nombrePostes: 1,
+        descriptionStage: '',
+        remuneration: 0,
+      });
+
+    } catch (error) {
+      console.log(error);
+      alert("Une erreur est survenue lors de l'ajout de l'employeur.");
+    }
+  }
+
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Nom de la personne contact:
-          <input
-            type="text"
-            value={nomContact}
-            onChange={(e) => setNomContact(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Courriel de la personne contact:
-          <input
-            type="email"
-            value={courrielContact}
-            onChange={(e) => setCourrielContact(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Numéro de téléphone de la personne contact:
-          <input
-            type="tel"
-            value={telephoneContact}
-            onChange={(e) => setTelephoneContact(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Nom de l'entreprise:
-          <input
-            type="text"
-            value={nomEntreprise}
-            onChange={(e) => setNomEntreprise(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Adresse de l'entreprise:
-          <input
-            type="text"
-            value={adresseEntreprise}
-            onChange={(e) => setAdresseEntreprise(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Type de stage:
-          <select value={typeStage} onChange={(e) => setTypeStage(e.target.value)} required>
-            <option value="">Sélectionner le type de stage</option>
-            <option value="Réseaux et sécurité">Réseaux et sécurité</option>
-            <option value="Développement d'applications">Développement d'applications</option>
-          </select>
-        </label>
-        <label>
-          Nombre de postes disponibles:
-          <input
-            type="number"
-            value={nombrePostes}
-            onChange={(e) => setNombrePostes(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Description du stage:
-          <textarea
-            value={descriptionStage}
-            onChange={(e) => setDescriptionStage(e.target.value)}
-            required
-          ></textarea>
-        </label>
-        <label>
-          Rémunération:
-          <input
-            type="text"
-            value={remuneration}
-            onChange={(e) => setRemuneration(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Ajouter le stage</button>
-      </form>
-    </div>
+    <form onSubmit={buttonAjouterClick}>
+      <label>
+        Nom du contact :
+        <input
+          type="text"
+          name="nomContact"
+          value={donneesEmployeur.nomContact}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      
+      <label>
+        Courriel du contact :
+        <input
+          type="email"
+          name="courrielContact"
+          value={donneesEmployeur.courrielContact}
+          onChange={handleChange}
+          required
+        />
+      </label>
+
+      <label>
+        Téléphone du contact :
+        <input
+          type="text"
+          name="telephoneContact"
+          value={donneesEmployeur.telephoneContact}
+          onChange={handleChange}
+          required
+        />
+      </label>
+
+      <label>
+        Nom de l'entreprise :
+        <input
+          type="text"
+          name="nomEntreprise"
+          value={donneesEmployeur.nomEntreprise}
+          onChange={handleChange}
+          required
+        />
+      </label>
+
+      <label>
+        Adresse de l'entreprise :
+        <input
+          type="text"
+          name="adresseEntreprise"
+          value={donneesEmployeur.adresseEntreprise}
+          onChange={handleChange}
+          required
+        />
+      </label>
+
+      <label>
+        Type de stage :
+        <select
+          name="typeStage"
+          value={donneesEmployeur.typeStage}
+          onChange={handleChange}
+          required
+        >
+          <option></option>
+          <option>Réseaux et sécurité</option>
+          <option>Développement d’applications</option>
+        </select>
+      </label>
+
+      <label>
+        Nombre de postes :
+        <input
+          type="number"
+          name="nombrePostes"
+          value={donneesEmployeur.nombrePostes}
+          onChange={handleChange}
+          required
+        />
+      </label>
+
+      <label>
+        Description du stage :
+        <textarea
+          name="descriptionStage"
+          value={donneesEmployeur.descriptionStage}
+          onChange={handleChange}
+          required
+        />
+      </label>
+
+      <label>
+        Rémunération :
+        <input
+          type="number"
+          name="remuneration"
+          value={donneesEmployeur.remuneration}
+          onChange={handleChange}
+          required
+        />
+      </label>
+
+      <button type="submit">Ajouter l'employeur</button>
+
+
+    </form>
   );
 };
-            
-export default FormulaireAjoutStage;
+
+export default FormulaireAjoutEmployeur;
